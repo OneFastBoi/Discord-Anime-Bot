ebc3012d4ffbbb03007ec4bdbe826eae0bfa4158
Petr Šebela <58368455+OneFastBoi@users.noreply.github.com>
2021-02-27 00:20:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ import time,datetime
 class Scrape:
     def __init__(self):
         self.prefix = 'https://www13.9anime.to'
+        self.link2match = ''
+        self.name = ''
+        self.epLinkList = []
     
     def search(self,keyword):
         searchWord,url = '',''
@@ -19,28 +22,44 @@ class Scrape:
                 searchWord += '+'
 
         url = 'https://www12.9anime.to/search?keyword=' + searchWord
+        print(url)
         self.page = requests.get(url)
         self.getStatus()
     
     def getStatus(self):
         soup = BeautifulSoup(self.page.content, 'html.parser')
         self.animeList = soup.find_all('ul',{'class':'anime-list'})
-        self.epListRaw = self.animeList[0].find_all('div',{'class':'tag ep'})  
-        print('# Update complete')  
+        self.epListRaw = self.animeList[0].find_all('div',{'class':'tag ep'})
+        self.epLinkRaw = self.animeList[0].find_all('a',{'class':'name'},href = True)
+        self.epLinkList.clear()
+        for link in self.epLinkRaw: self.epLinkList.append(link['href'])
+        self.animeIndex = self.epLinkList.index(self.link2match)
+        print(self.animeIndex)
+
+    def getName(self,link):
+        nameSplit = link.split('/')
+        self.link2match = '/watch/' + nameSplit[nameSplit.index('watch')+1]
+        self.name = nameSplit[nameSplit.index('watch')+1].split('.')[0]
+        print(self.link2match)
 
 Scrape = Scrape()
-
 client = commands.Bot(command_prefix = '!')
 x = True
 
 @client.event
 async def on_ready():    
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Kimetsu no Yaiba: Mugen Train"))
-    anime_status.start()
-    print('# Core Ready')    
+    #anime_status.start()
+    print('# Core Ready')
+
+@client.command(aliases =['add'])
+async def addAnime(ctx,link):
+    #https://www13.9anime.to/watch/horimiya.m0o8/ep-1
+    Scrape.getName(link)
+    Scrape.search(Scrape.name)
 
 @tasks.loop(seconds=60)
-async def anime_status():
+async def animeStatus():
     podel = client.get_channel(814250079636553788)
     global x
     try:
